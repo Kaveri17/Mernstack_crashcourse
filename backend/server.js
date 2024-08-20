a19eb1af42da2005b85dc4893bfbaa178795cc4b
kaveripunu <kaveripunu@gmail.com>
2024-08-20 21:09:10 +0545

    first commit

---
+++ backend/server.js
@@ -0,0 +1,29 @@
+import express from 'express'
+import dotenv from "dotenv"
+import { connectDB } from './config/db.js';
+import path from "path";
+import productRoute from './routes/productRoute.js'
+
+dotenv.config();
+
+const app = express()
+const PORT = process.env.PORT || 8000;
+
+const __dirname = path.resolve();
+
+// allows us to accept JSON data in the req.body
+app.use(express.json());
+
+app.use("/api/products", productRoute);
+
+if (process.env.NODE_ENV === "production") {
+	app.use(express.static(path.join(__dirname, "/frontend/dist")));
+	app.get("*", (req, res) => {
+		res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"));
+	});
+}
+
+app.listen(8000, () =>{
+    connectDB()
+    console.log("Server started at http://localhost:" + PORT)
+})
